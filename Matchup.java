e1826a73594db8382b2994e8b6ab8051f28fb50d
Sam Schmitz <44587628+SamSchmitz98@users.noreply.github.com>
2021-01-06 14:36:24 -0600

    Create Matchup.java

---
+++ Matchup.java
@@ -0,0 +1,44 @@
+package scheduler;
+
+public class Matchup {
+
+	Team home;
+	Team away;
+	int week;
+	
+	Matchup(int week, Team home, Team away){
+		this.week = week;
+		this.home = home;
+		this.away = away;
+	}
+	
+	Matchup(int week, Team home){
+		this.week = week;
+		this.home = home;
+		this.away = null;
+	}
+	
+	Team getHome() {
+		return home;
+	}
+	
+	Team getAway() {
+		return away;
+	}
+	
+	int getWeek() {
+		return week;
+	}
+	
+	boolean isBye() {
+		return away == null;
+	}
+	
+	boolean isConference() {
+		if (isBye()) {
+			return false;
+		}
+		return home.getConferenceID() == away.getConferenceID();
+	}
+	
+}
